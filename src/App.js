1122a2a19837014b2af5f168891e327cf913e0d6
dm-mensavi <mensavidavid@gmail.com>
2023-09-28 01:05:01 +0000

    Setup environment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+import About from './components/About';
+import Articles from './components/Articles';
+import Campaign from './components/Campaign';
+import Copyright from './components/Copyright';
+import Footer from './components/Footer';
+import Newsletter from './components/Newsletter';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Contact />
+      <Navbar />
+      <About />
+      <Articles />
+      <Campaign />
+      <Copyright />
+      <CTA />
+      <FAQ />
+      <Footer />
+      <Hero />
+      <Newsletter />
+      <Testimonials />
     </div>
   );
 }
